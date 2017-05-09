47ee32fe67b61be107395f42ffd557b5dbc1b3c0
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-05-09 11:35:03 -0300

    Fix structure

--- crystal/src/generator.cr
+++ crystal/src/generator.cr
@@ -2,7 +2,7 @@ require "./generator/prime_sieve.cr"
 
 if ARGV[0]?
   s = PrimeSieve.eratosthenes(ARGV[0].to_i)
-  puts "Generated #{s.size} primes"
+  puts "#{s.size}"
 else
   puts "Please enter a max number of integers to explore"
 end
